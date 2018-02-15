dfce83e192cfa8c31034cb90d01576e4fa2ffd15
loksly <loksly@gmail.com>
2018-02-15 14:30:14 +0100

    Simpler code, Better code

--- index.js
+++ index.js
@@ -2,15 +2,14 @@
 	'use strict';
 
 	const fs = require('fs'),
+		path = require('path'),
 		sql = require('mssql'),
 		redis = require('redis'),
 		Q = require('q'),
 		schedule = require('node-schedule'),
 		readline = require('readline'),
 		parseString = require('xml2js').parseString,
-		lib = {
-			'util': require('./routes/util')
-		};
+		lib = require('require.all')('./routes');
 
 	const rl = readline.createInterface({
 		input: process.stdin,
@@ -20,17 +19,13 @@
 
 	const config = require('./config.json');
 
-	const connectiondeferred = Q.defer();
-	const jobsdeferred = Q.defer();
-
 	const redisclient = redis.createClient(config.redis.port, config.redis.host);
 	const redispublish = redis.createClient(config.redis.port, config.redis.host);
-	const connection = new sql.Connection(config.db, connectiondeferred.makeNodeResolver());
 
-	let jobs = [];
-
-	fs.readFile('jobs.xml', 'utf8', jobsdeferred.makeNodeResolver());
-
+	let jobs = [],
+		crons = [],
+		connection = false;
+	
 	redisclient.on('error', function (err) {
 		logger.error('Error REDIS:', err);
 	});
@@ -45,9 +40,9 @@
 				
 				let newkey = key;
 				if (typeof params === 'object'){
-					for (const p in params){
-						newkey = newkey.replace(':' + p, params[p]);
-					}
+					newkey = Object.keys(params).reduce(function(p, c){
+						return p.replace(':' + c, params[c]);
+					}, newkey);
 				}
 				logger.log(newkey);
 				redisclient.set(newkey, JSON.stringify(value), redis.print);
@@ -91,7 +86,7 @@
 				});
 
 				fns.reduce(Q.when, Q(0)).then(function(){
-					//logger.log('todas terminadas');
+					//logger.log('Every parameter option has been runned');
 				}).fail(function(err){
 					logger.error('fallo', err);
 				});
@@ -123,7 +118,7 @@
 			const fn = caller(functionname, obj, job.$.key, parameters);
 			fn();
 		} else {
-			logger.error('Wrong command:', cmd);
+			logger.error('Wrong command:', cmd, 'Enter help for available commands');
 		}
 	}
 
@@ -132,59 +127,143 @@
 		process.exit(0);
 	});
 
+	function connect(){
+		const connectiondeferred = Q.defer();
+		connection = new sql.Connection(config.db, connectiondeferred.makeNodeResolver());
+		
+		return connectiondeferred.promise;
+	}
+
+	function loadXML(){
+		const jobsdeferred = Q.defer();
+
+		fs.readFile(path.join(__dirname, 'jobs.xml'), 'utf8', jobsdeferred.makeNodeResolver());
+
+		return jobsdeferred.promise;
+	}
+
+	function xml2jobs(xml){
+		const paserDefer = Q.defer();
+
+		parseString(xml, paserDefer.makeNodeResolver());
+
+		return paserDefer.promise;
+	}
+
+	function cancelAllCrons(){
+		crons.forEach(function(c){
+			c.cancel();
+		});
+		crons = [];
+	}
+
+	function setJobs(jbs){
+		cancelAllCrons();
+
+		jobs = jbs.jobs.job;
+		crons = jobs.map(function(job){
+			const cronmatching = job.$.cron;
+			const methodname = job.$.method.split('.');
+			const obj = lib[methodname[0]],
+				functionname = typeof lib[methodname[0]][methodname[1]] === 'undefined' ? false : lib[methodname[0]][methodname[1]];
+			let parameters = false;
+
+			if (!obj || !functionname){
+				logger.error('Bad configuration!', methodname[0], methodname[1]);
+
+				return false;
+			}
+
+			if (job.parameters){
+				parameters = job.parameters[0].field.reduce(function(prev, parameter){
+					prev[parameter.$.name] = parameter.value;
+
+					return prev;
+				}, {});
+			}
+			const fn = caller(functionname, obj, job.$.key, parameters);
+
+			return schedule.scheduleJob(cronmatching, fn);
+		}).filter(function(a){ return a; });
+	}
+
+	Q.all([loadXML(), connect()]).then(function(content){
+
+		return xml2jobs(content[0]);
+	}).then(function(jbs){
+		setJobs(jbs);
+		rl.prompt();
+	}).catch(function(err){
+		logger.error(err);
+		process.exit(-1);
+	});
+
+	process.on('exit', function(){
+		cancelAllCrons();
+	});
+
+
+	const supportedCommandsDescription = {
+		'reload': 'Reload jobs file',
+		'runall': 'Runs all available commands',
+		'help': 'Show the list of available commands',
+		'?': 'Help alias',
+		'quit': 'Exits'
+	};
+
+	const supportedCommands = {
+		reload: function(){
+			return loadXML().then(function(content){
+
+				return xml2jobs(content);
+			}).then(function(jbs){
+				setJobs(jbs);
+				logger.log('OK');
+			}).catch(function(err){
+				logger.error(err);
+			});
+		},
+		runall: function(){
+			jobs.map(function(job){
+				return job.$.key;
+			}).forEach(function(c){
+				runCommand(c);
+			});
+		},
+		help: function(){
+			const comms = Object.keys(supportedCommands).map(function(s){
+				return s + (typeof supportedCommandsDescription[s] === 'string' ? "\n\t" + supportedCommandsDescription[s] : '');
+			});
+			const options = jobs.map(function(job){
+				return job.$.key + (job.description ? "\n\t" + job.description : '');
+			}).concat(comms);
+
+			logger.log("\n", 'The available options are:', "\n");
+			logger.log(options.join("\n"));
+		},
+		'?': function(){
+			supportedCommands.help();
+		},
+		quit: function(){
+			connection.close();
+			process.exit(0);
+		}
+	};
+
 	rl.on('line', function(line){
 		if (line.trim() !== ''){
-			runCommand(line.trim());
+			const prefix = line.split(' ')[0];
+
+			if (typeof supportedCommands[prefix] === 'function'){
+				supportedCommands[prefix](line);
+			} else {
+				runCommand(line.trim());
+			}
 		}
 		rl.prompt();
 	}).on('close', function(){
 		process.exit(0);
 	});
 
-	Q.all([jobsdeferred.promise, connectiondeferred.promise]).then(function(content){
-
-		parseString(content[0], function(err, jbs) {
-			if (err){
-				logger.error(err);
-				process.exit(-1);
-			}
-			if (process.argv.length > 2){
-				runCommand(process.argv[2]);
-			} else {
-				jobs = jbs.jobs.job;
-				const crons = jobs.map(function(job){
-
-					const cronmatching = job.$.cron;
-					const methodname = job.$.method.split('.');
-					const obj = lib[methodname[0]],
-						functionname = lib[methodname[0]][methodname[1]];
-					let parameters = false;
-
-					if (job.parameters){
-						parameters = job.parameters[0].field.reduce(function(prev, parameter){
-							prev[parameter.$.name] = parameter.value;
-
-							return prev;
-						}, {});
-					}
-
-					const fn = caller(functionname, obj, job.$.key, parameters);
-
-					return schedule.scheduleJob(cronmatching, fn);
-				});
-
-				process.on('exit', function(){
-					crons.forEach(function(c){
-						c.cancel();
-					});
-				});
-			}
-
-			rl.prompt();
-		});
-
-	}).fail(function(err){
-		logger.error(err);
-	});
 
 })(process, console);
