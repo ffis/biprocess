9d05dbcdf6dfe6e6780f27e45497d7116d9fa529
loksly <loksly@gmail.com>
2019-11-11 13:55:47 +0100

    [IMPROVEMENT] Code linting

--- routes/mongo.js
+++ routes/mongo.js
@@ -1,10 +1,10 @@
 (function(module){
-	'use strict';
+    'use strict';
 
     const findAndReturnAsPromise = require('../lib/mongo').findAndReturnAsPromise;
     const aggregateAndReturnAsPromise = require('../lib/mongo').aggregateAndReturnAsPromise;
 
-	module.exports.genericFind = function(parameters){
+    module.exports.genericFind = function(parameters) {
 
         const client = parameters.mongodbclient,
             dbname = parameters.dbname,
@@ -13,17 +13,17 @@
             filter = parameters.filter || '{}',
             sort = parameters.sort || '{}';
 
-		return findAndReturnAsPromise(client, dbname, collection, JSON.parse(project), JSON.parse(filter), JSON.parse(sort));
-	};
+        return findAndReturnAsPromise(client, dbname, collection, JSON.parse(project), JSON.parse(filter), JSON.parse(sort));
+    };
 
-    module.exports.genericAggregate = function(parameters){
+    module.exports.genericAggregate = function(parameters) {
 
         const client = parameters.mongodbclient,
             dbname = parameters.dbname,
             collection = parameters.collection,
             pipeline = parameters.pipeline || '[]';
 
-		return aggregateAndReturnAsPromise(client, dbname, collection, JSON.parse(pipeline));
-	};
+        return aggregateAndReturnAsPromise(client, dbname, collection, JSON.parse(pipeline));
+    };
 
 })(module);
